5e50f7df350642f8084667289cff03e4ac814ade
Gal Halevi <111064249+gal-halevi-wdc@users.noreply.github.com>
2023-04-27 22:41:14 +0300

    Create generate_workflow.py

---
+++ generate_workflow.py
@@ -0,0 +1,28 @@
+import json
+from ruamel.yaml.comments import CommentedMap as ordereddict
+from ruamel.yaml import YAML
+from pathlib import Path
+
+yaml_file = Path("checkout_template.yml")
+yml_file = YAML()
+yml_file.default_flow_style = True
+template = yml_file.load(yaml_file)
+
+with open("manifest.json") as manifest_file:
+    manifest = json.load(manifest_file)
+
+steps = []
+for component, component_data in manifest.items():
+    component_dict = ordereddict()
+    component_dict["uses"] = "actions/checkout@v3"
+    component_dict["with"] = ordereddict({
+        "repository": component_data["src"],
+        "path": component
+    })
+    steps.append(component_dict)
+
+template["jobs"]["Checkout-Manifest"]["steps"] = steps
+
+yml_file = YAML()
+yaml_file = Path(".github/workflows/checkout_manifest.yml")
+yml_file.dump(template, yaml_file)
